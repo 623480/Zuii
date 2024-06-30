eb88c1ce4ff0c45f914c5f44be1aaad035fffb93
Sravan29 <sanamsravan972001@gmail.com>
2024-06-30 12:56:33 +0530

    Added About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import User from './User'
+import React from "react";
+import ZUIII_LOGO from "../assets/ZUIII_LOGO.png";
+
 const About = () => {
   return (
-    <div className='min-h-lvh'>
-      <User peru={"sravan"}/>
+    <div className="min-h-lvh">
+      <div className="w-3/12 my-6 m-auto px-8 text-center">
+        <img src={ZUIII_LOGO} alt="logo" />
+        <p className="text-sm font-medium font-serif">
+          The ZUIII is inspired by childhood nostalgia. It was the word that
+          excited children the most when they sat on bikes with their dad,
+          whether they were in motion or at rest. They felt like they were
+          driving and would mimic the sound, saying "ZUIII... ZUIII..."
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
