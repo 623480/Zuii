868f9a88d7e83c82106ffa3a57da646ea981de51
Sravan29 <sanamsravan972001@gmail.com>
2024-07-02 21:48:48 +0530

    updated-shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Shimmer = () => {
   return (
-    <div className="flex flex-row flex-wrap h-lvh m-5 mt-16">
+    <div className="flex flex-row flex-wrap min-h-lvh m-5 mt-16">
       
       
       <div className="w-[260px] h-64 rounded-lg bg-gray-300 mx-4 my-4"></div>
