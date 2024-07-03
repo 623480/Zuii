1e110db355351509bd85747242f54b77bac06ca1
Sravan29 <sanamsravan972001@gmail.com>
2024-07-03 22:47:06 +0530

    updated constants file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,7 +5,7 @@ export const LOGO_URL =
   "https://dynamic.brandcrowd.com/asset/logo/94def15e-5330-48a2-9ed6-4992a21f4b7f/insta-square?logoTemplateVersion=1&v=637682366529570000";
 
 export const SWIGGY_API_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&is-seo-widgets-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&is-seo-widgets-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=";
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=";
