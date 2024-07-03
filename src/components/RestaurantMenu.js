810537f3ef6678018ffbda015549933dcf6fa8e3
Sravan29 <sanamsravan972001@gmail.com>
2024-07-03 22:26:38 +0530

    updated Restaurant Menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useParams } from 'react-router-dom';
-import RestaurantMenuShimmer  from './RestaurantMenuShimmer';
-import { useRestaurantMenu } from '../Hooks/useRestaurantMenu';
-import RestaurantCategory from './RestaurantCategory';
+import { useParams } from "react-router-dom";
+import RestaurantMenuShimmer from "./RestaurantMenuShimmer";
+import { useRestaurantMenu } from "../Hooks/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
+// import { useCheckRestaurantMenuData } from "../Hooks/useCheckRestaurantMenuData";
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const [showIndex, setShowIndex] = useState(null);
 
   const restaurantInfo = useRestaurantMenu(resId);
-  console.log(restaurantInfo);
+  const CheckRestaurantMenuData = (restaurantInfo) => {
+    for (let i = 0; i < restaurantInfo.cards.length; i++) {
+      let checkData =
+        restaurantInfo?.cards[i]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+      if (checkData !== undefined) {
+        return checkData;
+      }
+    }
+  };
 
   if (restaurantInfo === null) return <RestaurantMenuShimmer />;
 
-  const categories =
-    restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
-      (category) =>
-        category.card.card['@type'] ===
-        'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
-    );
+  const cards = CheckRestaurantMenuData(restaurantInfo);
+  const categories = cards.filter(
+    (category) =>
+      category.card.card["@type"] ===
+      "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+  );
 
   return (
-    <div className='text-center min-h-lvh my-4'>
-      <div className='my-8 py-2'>
-      <h1 className='text-lg font-bold'>{restaurantInfo?.cards[2]?.card?.card?.info?.name}</h1></div>
+    <div className="text-center min-h-lvh my-4">
+      <div className="my-8 py-2">
+        <h1 className="text-lg font-bold">
+          {restaurantInfo?.cards[2]?.card?.card?.info?.name}
+        </h1>
+      </div>
       <div>
         {categories.map((category, index) => (
           <RestaurantCategory
